8397ed097ddac92df01b2da97f2a363a9c999164
Seth Bowman <brandonsethbowman@gmail.com>
2023-07-19 13:28:35 -0500

    Example added

---
+++ TypesExample/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace TypesExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            // Type - name - value
+            int whatever = 100;
+            //ints are whole numbers
+
+            double number = 10.5;
+
+            decimal cash = 500.00m;
+
+            string myWord = "hello class 123";
+            
+            char initial = 'a';
+
+            bool isAlive = true;
+
+
+            //VALUE TYPES
+
+            int a = 1;
+            int b = a;
+
+            a = 500;
+
+            //REFERENCE TYPES
+
+            var p1 = new Point();
+
+            p1.X = 1;
+
+            var p2 = p1;
+
+            p2.X = 500;
+
+            Console.WriteLine(p1.X);
+            Console.WriteLine(p2.X);
+        }
+    }
+}
